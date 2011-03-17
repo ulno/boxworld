df7686e14fe7bc71456d2ea149270f8e516fce4c
chris.willmore@yahoo.com <chris.willmore@yahoo.com@445e7b20-2786-1b12-35be-a1b6363c6c07>
2011-03-17 23:25:34 +0000

    Added mpi-powered distributed computing. Works for two connected boxes, one in each process.

--- src/boxworld/Inbox.py
+++ src/boxworld/Inbox.py
@@ -5,14 +5,16 @@ Created on Mar 4, 2011
 '''
 
 #import Inbox_Content.Inbox_Content as Inbox_Content
+from MpiChannel import Envelope
+
 from Transport import Transport
 
 from Queue import Queue
-
-class Inbox(object): # TODO: must probably inherit from channel
+    
+class LocalInbox(): # TODO: must probably inherit from channel
     
 
-    def __init__(self, queue_names):
+    def __init__(self):
         
         self.queue = Queue()
 
@@ -26,4 +28,21 @@ class Inbox(object): # TODO: must probably inherit from channel
     
     def take(self):
         return self.queue.get(block=True)
+    
+class RemoteInbox():
+    
+
+    def __init__(self, srcCoords, destCoords, mpiChannel):
+        
+        self.srcCoords = srcCoords
+        self.destCoords = destCoords
+        self.mpiChannel = mpiChannel
+
+    
+    def put(self, obj):
+        print ("Sending from %s to %s" %(str(self.srcCoords), str(self.destCoords)))
+        self.mpiChannel.send(Envelope(self.srcCoords, self.destCoords, obj))
+    
+    def take(self):
+        print "Trying to take from remote"
         
