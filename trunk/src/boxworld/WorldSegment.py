e74ef89cfd130e7a4607f8539053d3c3f3e343b7
chris.willmore@yahoo.com <chris.willmore@yahoo.com@445e7b20-2786-1b12-35be-a1b6363c6c07>
2011-03-31 10:09:30 +0000

    Adding initial parsing of boxes. Only supports defing initial terpene level

--- trunk/src/boxworld/WorldSegment.py
+++ trunk/src/boxworld/WorldSegment.py
@@ -1,6 +1,9 @@
 import re
 from .Source import Source
 from .Chemical_Sink import Chemical_Sink
+from .Geometry import Dimensions
+from .Geometry import Coord
+from .Box import Box
 
 class WorldSegmentFactory:
     
@@ -14,18 +17,22 @@ class WorldSegmentFactory:
         self.sinks = []
         self.boxes = []
 
-
         self.importFromFile()
 
-    COMMENT_REGEX = "(#.*)?$" #defined as optional
-    EMPTY_LINE_REGEX = "\s*" + COMMENT_REGEX
+    COMMENT_REGEX = "\s*(#.*)?$" #defined as optional
+    EMPTY_LINE_REGEX = COMMENT_REGEX
     TIME_START_REGEX = "time_start\s+(\d+)" + COMMENT_REGEX
     TIME_END_REGEX = "time_end\s+(\d+)" + COMMENT_REGEX
     TIME_DELTA_REGEX = "time_delta\s+(\d+)" + COMMENT_REGEX
+    CUBE_SIZE_REGEX = "cube_size\s+(\d+)" + COMMENT_REGEX
+    DIMENSIONS_REGEX = "dimensions\s+(\d+),(\d+),(\d+)\s*" + COMMENT_REGEX
+    
     
     SOURCE_REGEX = "(\d+),(\d+),(\d+)\s*" + COMMENT_REGEX
     
     SINK_REGEX = "(\d+),(\d+),(\d+),(\d+),(\d+)\s*" + COMMENT_REGEX
+
+    BOX_REGEX = "(\d+)\s*" + COMMENT_REGEX
 
     #Parse States
     PARSE_WORLD_SETTING = 'parseWorldSetting'
@@ -43,6 +50,8 @@ class WorldSegmentFactory:
         
         self.parseState = 'parseWorldSetting'
         
+        self.currentCoord = Coord(0,0,0)
+        
         for line in file:
             if re.match(self.EMPTY_LINE_REGEX, line):
                 continue
@@ -58,6 +67,28 @@ class WorldSegmentFactory:
                 self.parseBox(line)
             else:
                 raise Exception("Could not parse line='%s'" % line)
+           
+    def parseBox(self, line):    
+        m = re.match(self.BOX_REGEX, line)
+        if not m:
+            raise Exception("Illegal Box syntax in line = '%s'" % line)
+    
+        tempFunc = lambda x: 1
+        lightFunc = lambda x: 1
+        
+        self.boxes.append(Box(self.currentCoord, self.cubeSize, lightFunc, 
+                              tempFunc,self.timeDelta, self.timeStart, 
+                              self.timeEnd, int(m.group(1)))) 
+    
+        # Increment coordinate for next parsed box
+        # The increment order in x, then y, then z. 
+        # The values wrap in that order when they hit the max, as defined by self.dimensions   
+        self.currentCoord.x = (self.currentCoord.x + 1) % self.dimensions.x
+        if self.currentCoord.x == 0:
+            self.currentCoord.y = (self.currentCoord.y + 1) % self.dimensions.y
+            if self.currentCoord.y == 0:
+                # Z is the last to increment and thus should never wrap
+                self.currentCoord.z = self.currentCoord.z + 1
             
     def parseSink(self, line):        
         m = re.match(self.SINK_REGEX, line)
@@ -112,7 +143,17 @@ class WorldSegmentFactory:
         m = re.match(self.TIME_DELTA_REGEX, line)
         if m:
             self.timeDelta = int(m.group(1))
-            return   
+            return
+        
+        m = re.match(self.CUBE_SIZE_REGEX, line)
+        if m:
+            self.cubeSize = int(m.group(1))
+            return
+        
+        m = re.match(self.DIMENSIONS_REGEX, line)
+        if m:
+            self.dimensions = Dimensions(int(m.group(1)), int(m.group(2)), int(m.group(3)))
+            return
         
     def getWorldSegment(self, rank):
         assert rank >= 0 and rank < self.worldSize
--- trunk/test/WorldSegmentTest.py
+++ trunk/test/WorldSegmentTest.py
@@ -1,6 +1,7 @@
 import unittest
 from boxworld.WorldSegement import WorldSegment
 from boxworld.WorldSegment import WorldSegmentFactory
+from boxworld.Geometry import Dimensions
 
 class WorldSegementTest(unittest.TestCase):
 
@@ -16,6 +17,9 @@ class WorldSegementTest(unittest.TestCase):
         self.assertEqual(factory.timeStart, 0)
         self.assertEqual(factory.timeEnd, 1000)
         self.assertEqual(factory.timeDelta, 1)
+        self.assertEqual(factory.cubeSize, 1)
+        self.assertEqual(factory.dimensions, Dimensions(2,2,2))
+        self.assertEqual(factory.dimensions.volume(), 8)
         
         self.assertEqual(len(factory.sources), 3)
         self.assertEqual(factory.sources[1].light, 5)
@@ -23,7 +27,7 @@ class WorldSegementTest(unittest.TestCase):
         self.assertEqual(factory.sources[1].e0, 10)
         
         self.assertEqual(len(factory.sinks), 3)
-        7,4,5,1,4
+        #7,4,5,1,4
         
         self.assertEqual(factory.sinks[1].terpene_concentration,7)
         self.assertEqual(factory.sinks[1].temperature, 277.15)
@@ -31,7 +35,7 @@ class WorldSegementTest(unittest.TestCase):
         self.assertEqual(factory.sinks[1].nox,1)
         self.assertEqual(factory.sinks[1].oh, 4)
         
-        self.assertEqual(len(factory.boxes), 0)
+        self.assertEqual(len(factory.boxes), 8)
 
 if __name__ == '__main__':
     unittest.main()
