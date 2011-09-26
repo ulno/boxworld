61148f441cb4ee8c9b2c50e5d94b9562fbad2324
chris.willmore@yahoo.com <chris.willmore@yahoo.com@445e7b20-2786-1b12-35be-a1b6363c6c07>
2011-09-26 09:04:56 +0000

    Added UT

---
+++ trunk/test/unit/SinkTest.py
@@ -0,0 +1,30 @@
+import unittest
+from boxworld.Box import Box
+from boxworld.Source import Source
+from boxworld.Geometry import Coord
+from boxworld.Sink import Sink
+
+class SinkTest(unittest.TestCase):
+    
+    def testCanonical(self):
+        '''
+        Test 1 box with 1 sink for 10 time steps.
+        Assert concentration is 1 at the end of the run.
+        '''
+        
+        nair = 101325 * 6.022e23 / (8.314 * (25 + 273.15))
+        fnair = nair * 1e-6 * 1e-9
+        
+        box = Box(position=Coord(0,0,0), 
+                  cube_length=1, 
+                  light_time_function = lambda t : 1, #always return 1
+                temperature_time_function = lambda t : 1, #always return 1 
+                timedelta=1, 
+                initial_time=0, 
+                end_time=10, 
+                initial_terpene=1, 
+                sinks=(Sink(5.0e11,30,15*fnair,2*fnair,1e-4*fnair),))
+        
+        box.run().join()
+        
+        self.assertEqual(box.terpene_concentration, 1)
