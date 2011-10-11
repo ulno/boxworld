d9a8dc75189af45150073ba13dd886179c71ca2c
Steffen.M.Noe@gmail.com <Steffen.M.Noe@gmail.com@445e7b20-2786-1b12-35be-a1b6363c6c07>
2011-10-11 08:47:38 +0000

    source got dilution parameter, sink parmeters ok, test are running

--- src/boxworld/Source.py
+++ src/boxworld/Source.py
@@ -24,18 +24,20 @@ class Source(object):
 	
 	#__slots__ = ['light', 'temperature', 'baseEmission', 'emission']
 	
-	def __init__(self, light, temperature, baseEmission):
-		self.light       = light		# actual light flux in mumol m-2 s-1
-		self.temperature = temperature	# actual temperature in degree Celsius
-		self.e0          = baseEmission	# species specific basal emission rate in conc per time
-		self.alpha		 = 0.0027		# empirical parameter
-		self.CL1         = 1.066		# empirical parameter
-		self.R			 = 8.314		# universal gas constant in J mol-1 K-1
-		self.Ts			 = 303.0    	# standard temperature (30 C) in K
-		self.TM			 = 314.0		# in K
-		self.CT1		 = 95.0E3		# empirical parameter in J mol-1
-		self.CT2		 = 230.0E3		# empirical parameter in J mol-1
-		self.emission    = self.guenther(light, temperature)
+	def __init__(self, 
+		#light, temperature, 
+		baseEmission):
+		#self.light       = light		# actual light flux in mumol m-2 s-1
+		#self.temperature = temperature		# actual temperature in degree Celsius
+		self.e0           = baseEmission	# species specific basal emission rate in conc per time
+		self.alpha	  = 0.0027		# empirical parameter
+		self.CL1          = 1.066		# empirical parameter
+		self.R		  = 8.314		# universal gas constant in J mol-1 K-1
+		self.Ts		  = 303.0    		# standard temperature (30 C) in K
+		self.TM		  = 314.0		# in K
+		self.CT1	  = 95.0E3		# empirical parameter in J mol-1
+		self.CT2	  = 230.0E3		# empirical parameter in J mol-1
+		#self.emission    = self.guenther(light, temperature)
 
 	
 	def guenther(self, light, temperature):
@@ -46,8 +48,7 @@ class Source(object):
 	 	functions CL and CT that modulate that basal rate according to the light and
 	 	temperature. 
 		'''
-		self.emission = self.e0 * self.CL(light) * self.CT(temperature)
-		return self.emission
+		return self.e0 * self.CL(light) * self.CT(temperature)
 		
 	def CL(self, light):
 		return self.alpha * self.CL1 * light / np.sqrt(1 + self.alpha**2 * light**2)
@@ -57,10 +58,11 @@ class Source(object):
 		return np.exp(self.CT1 * (T - self.Ts) / (self.R * self.Ts * T)) / \
 			(1 + np.exp(self.CT2 * (T - self.TM) / (self.R * self.Ts * T)))
 	
-	def getEmission(self):
+	def getEmission(self, light, temperature, fnair, time_delta):
 		'''
 		Accessor method to read out the emission from that source.
 		'''
-		return self.emission
+		# print "fnair ", fnair
+		return self.guenther(light, temperature) * fnair * 1e-6 * time_delta # 1e-6 = dilution factor into volume!
 		
 		
